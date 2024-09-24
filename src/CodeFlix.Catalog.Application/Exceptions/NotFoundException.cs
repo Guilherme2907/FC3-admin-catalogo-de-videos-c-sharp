346627cc9839be22a606d31d64eb5dc779e01927
Guilherme2907 <guilherme.magalhaes2907@gmail.com>
2024-09-24 11:18:25 -0300

    Creation of exceptions classes to handle application exceptions.

---
+++ src/CodeFlix.Catalog.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,15 @@
+﻿namespace CodeFlix.Catalog.Application.Exceptions;
+
+public class NotFoundException : ApplicationException
+{
+    public NotFoundException(string? message) : base(message)
+    { }
+
+    public static void ThrowIfNull(object? value, string message)
+    {
+        if (value is null)
+        {
+           throw new NotFoundException(message);
+        }
+    }
+}
