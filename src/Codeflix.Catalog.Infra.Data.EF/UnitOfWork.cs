8650e549173e7ed388dbf9262eb538cffbb8df3c
Guilherme2907 <guilherme.magalhaes2907@gmail.com>
2024-09-24 11:27:17 -0300

    Implementation of UnitOfWork

---
+++ src/Codeflix.Catalog.Infra.Data.EF/UnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using Codeflix.Catalog.Infra.Data.EF.Repositories;
+using CodeFlix.Catalog.Application.Interfaces;
+
+namespace Codeflix.Catalog.Infra.Data.EF;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly CodeflixCatalogDbContext _context;
+
+    public UnitOfWork(CodeflixCatalogDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task Commit(CancellationToken cancellationToken)
+    {
+        await _context.SaveChangesAsync();
+    }
+
+    public Task Rollback(CancellationToken cancellationToken)
+        => Task.CompletedTask;
+}
