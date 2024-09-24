c9b44a0074fe767938f5795bf862269937993157
Guilherme2907 <guilherme.magalhaes2907@gmail.com>
2024-09-24 11:21:36 -0300

    Creation of the "ListCategories" usecase.

---
+++ src/CodeFlix.Catalog.Application/UseCases/Category/ListCategories/IListCategories.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+
+namespace CodeFlix.Catalog.Application.UseCases.Category.ListCategories;
+
+public interface IListCategories : IRequestHandler<ListCategoriesInput, ListCategoriesOutput>
+{
+}
