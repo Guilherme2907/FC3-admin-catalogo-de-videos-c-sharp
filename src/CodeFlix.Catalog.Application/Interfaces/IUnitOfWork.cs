4ba0a6ccec6d288fb664380dc39901c4293de15f
Guilherme2907 <guilherme.magalhaes2907@gmail.com>
2024-09-24 11:19:08 -0300

    Creation of IUnitOfWork interface, to be implemented by the infra layer.

---
+++ src/CodeFlix.Catalog.Application/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,8 @@
+﻿namespace CodeFlix.Catalog.Application.Interfaces;
+
+public interface IUnitOfWork
+{
+    public Task Commit(CancellationToken cancellationToken);
+
+    public Task Rollback(CancellationToken cancellationToken);
+}
