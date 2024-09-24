73f524a013196a8e5977e743c190afc95de74f04
Guilherme2907 <guilherme.magalhaes2907@gmail.com>
2024-09-24 11:30:36 -0300

    Creation of the "DeleteCategoryTest" unit test.

---
+++ tests/Codeflix.Catalog.UnitTests/Application/Category/DeleteCategory/DeleteCategoryTestFixture.cs
@@ -0,0 +1,16 @@
+﻿using Codeflix.Catalog.UnitTests.Application.Category.Common;
+using Codeflix.Catalog.UnitTests.Application.Category.CreateCategory;
+using Xunit;
+
+namespace Codeflix.Catalog.UnitTests.Application.Category.DeleteCategory;
+
+[CollectionDefinition(nameof(DeleteCategoryTestFixture))]
+public class DeleteCategoryTestFixtureCollection
+    : ICollectionFixture<DeleteCategoryTestFixture>
+{ }
+
+public class DeleteCategoryTestFixture : CategoryUseCasesBaseFixture
+{
+}
+
+
