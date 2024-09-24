ef902fdaffc4190006c833546d8712a3186ab555
Guilherme2907 <guilherme.magalhaes2907@gmail.com>
2024-09-24 11:26:12 -0300

    Creation of EF DbContext for the project

---
+++ src/Codeflix.Catalog.Infra.Data.EF/Repositories/CodeflixCatalogDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Codeflix.Catalog.Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Codeflix.Catalog.Infra.Data.EF.Repositories;
+
+public class CodeflixCatalogDbContext
+    : DbContext
+{
+    public DbSet<Category> Categories => Set<Category>();
+
+    public CodeflixCatalogDbContext(
+        DbContextOptions<CodeflixCatalogDbContext> options
+    ) : base(options)
+    { }
+}
