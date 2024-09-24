fa03b804bec556ce7407824d0802aa15c205bc5a
Guilherme2907 <guilherme.magalhaes2907@gmail.com>
2024-09-24 11:20:48 -0300

    Creation of the "CreateCategory" usecase.

---
+++ src/CodeFlix.Catalog.Application/UseCases/Category/CreateCategory/CreateCategoryInput.cs
@@ -0,0 +1,20 @@
+﻿using CodeFlix.Catalog.Application.UseCases.Category.Common;
+using MediatR;
+
+namespace CodeFlix.Catalog.Application.UseCases.Category.CreateCategory;
+
+public class CreateCategoryInput : IRequest<CategoryModelOutput>
+{
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public CreateCategoryInput(string name, string? description = null, bool isActive = true)
+    {
+        Name = name;
+        Description = description ?? string.Empty;
+        IsActive = isActive;
+    }
+}
---
+++ src/CodeFlix.Catalog.Application/UseCases/Category/CreateCategory/ICreateCategory.cs
@@ -0,0 +1,8 @@
+﻿using CodeFlix.Catalog.Application.UseCases.Category.Common;
+using MediatR;
+
+namespace CodeFlix.Catalog.Application.UseCases.Category.CreateCategory;
+
+public interface ICreateCategory : IRequestHandler<CreateCategoryInput, CategoryModelOutput>
+{
+}
