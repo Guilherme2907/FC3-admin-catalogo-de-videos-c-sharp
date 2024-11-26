dafc903942779d1ef479b0cba60b0d20ce2cf3fe
Guilherme2907 <guilherme.magalhaes2907@gmail.com>
2024-11-26 15:56:06 -0300

    Creation of classes to handle requests and responses using snake case pattern

---
+++ src/Codeflix.Catalog.Api/Extensions/String/StringSnakeCaseNamingStrategy.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json.Serialization;
+
+namespace Codeflix.Catalog.Api;
+
+public static class StringSnakeCaseNamingStrategy
+{
+    private readonly static NamingStrategy _snakeCaseNamingStrategy
+        = new SnakeCaseNamingStrategy();
+
+    public static string ToSnakeCase(this string stringToConvert)
+    {
+        ArgumentNullException.ThrowIfNull(stringToConvert, nameof(stringToConvert));
+
+        return _snakeCaseNamingStrategy.GetPropertyName(stringToConvert, false);
+    }
+}
