e0c6606caaace90f36852e6a650be887cd2b2d8a
Guilherme2907 <guilherme.magalhaes2907@gmail.com>
2024-11-26 15:52:20 -0300

    Creation of models to handle api responses

---
+++ src/Codeflix.Catalog.Api/ApiModels/Response/ApiResponseList.cs
@@ -0,0 +1,24 @@
+﻿using CodeFlix.Catalog.Application.Common;
+using CodeFlix.Catalog.Application.UseCases.Category.Common;
+
+namespace Codeflix.Catalog.Api.ApiModels.Response;
+
+public class ApiResponseList<TItemData> : ApiResponse<IReadOnlyList<TItemData>>
+{
+    public ApiResponseListMeta Meta { get; set; }
+
+    public ApiResponseList(IReadOnlyList<TItemData> data, int perPage, int currentPage, int total)
+        : base(data)
+    {
+        Meta = new ApiResponseListMeta(perPage, currentPage, total);
+    }
+
+    public ApiResponseList(PaginatedListOutput<TItemData> paginatedListOutput)
+        : base(paginatedListOutput.Items)
+    {
+        Meta = new ApiResponseListMeta( paginatedListOutput.PerPage,
+                                        paginatedListOutput.Page,
+                                        paginatedListOutput.Total
+                                      );
+    }
+}
---
+++ src/Codeflix.Catalog.Api/ApiModels/Response/ApiResponseListMeta.cs
@@ -0,0 +1,17 @@
+﻿namespace Codeflix.Catalog.Api.ApiModels.Response;
+
+public class ApiResponseListMeta
+{
+    public int PerPage { get; set; }
+
+    public int CurrentPage { get; set; }
+
+    public int Total { get; set; }
+
+    public ApiResponseListMeta(int perPage, int currentPage, int total)
+    {
+        PerPage = perPage;
+        CurrentPage = currentPage;
+        Total = total;
+    }
+}
