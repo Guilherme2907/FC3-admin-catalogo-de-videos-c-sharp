9b318606c35cd51d2464918d576b1e1e51db9a56
Guilherme2907 <guilherme.magalhaes2907@gmail.com>
2024-11-26 15:57:51 -0300

    Creation of configurations classes

---
+++ src/Codeflix.Catalog.Api/Configurations/UsecasesConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using CodeFlix.Catalog.Application.Interfaces;
+using Codeflix.Catalog.Domain.Repository;
+using Codeflix.Catalog.Infra.Data.EF.Repositories;
+using Codeflix.Catalog.Infra.Data.EF;
+using CodeFlix.Catalog.Application.UseCases.Category.CreateCategory;
+using MediatR;
+
+namespace Codeflix.Catalog.Api.Configurations;
+
+public static class UsecasesConfiguration
+{
+    public static IServiceCollection AddUseCases(this IServiceCollection services)
+    {
+        services.AddMediatR(typeof(CreateCategory));
+        services.AddRepositories();
+
+        return services;
+    }
+
+    private static IServiceCollection AddRepositories(
+           this IServiceCollection services
+       )
+    {
+        services.AddTransient<ICategoryRepository, CategoryRepository>();
+        services.AddTransient<IUnitOfWork, UnitOfWork>();
+        return services;
+    }
+}
