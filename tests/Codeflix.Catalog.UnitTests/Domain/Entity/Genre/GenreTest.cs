6f2e75f61509857315b3897e76e78c12abfa1392
Guilherme2907 <guilherme.magalhaes2907@gmail.com>
2024-11-27 13:25:13 -0300

    Creation of unit tests for Genre domain

---
+++ tests/Codeflix.Catalog.UnitTests/Domain/Entity/Genre/GenreTest.cs
@@ -0,0 +1,179 @@
+﻿using Xunit;
+using FluentAssertions;
+using DomainEntity = Codeflix.Catalog.Domain.Entity;
+using Codeflix.Catalog.Domain.Exceptions;
+
+namespace Codeflix.Catalog.UnitTests.Domain.Entity.Genre;
+
+[Collection(nameof(GenreTestFixture))]
+public class GenreTest
+{
+    private readonly GenreTestFixture _fixture;
+
+    public GenreTest(GenreTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact(DisplayName = nameof(Instantiate))]
+    [Trait("Domain", "Genre - Aggregates")]
+    public void Instantiate()
+    {
+        var genreName = _fixture.GetValidName();
+
+        var dateTimeBefore = DateTime.Now;
+        var genre = new DomainEntity.Genre(genreName);
+        var dateTimeAfter = DateTime.Now.AddSeconds(1);
+
+        genre.Should().NotBeNull();
+        genre.Name.Should().Be(genreName);
+        genre.CreatedAt.Should().NotBeSameDateAs(default);
+        (genre.CreatedAt >= dateTimeBefore).Should().BeTrue();
+        (genre.CreatedAt <= dateTimeAfter).Should().BeTrue();
+        genre.IsActive.Should().BeTrue();
+    }
+
+    [Theory(DisplayName = nameof(InstantiateWithIsActive))]
+    [Trait("Domain", "Genre - Aggregates")]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void InstantiateWithIsActive(bool isActive)
+    {
+        var genreName = _fixture.GetValidName();
+        var genre = new DomainEntity.Genre(genreName, isActive);
+
+        genre.Should().NotBeNull();
+        genre.Name.Should().Be(genreName);
+        genre.IsActive.Should().Be(isActive);
+    } 
+    
+    [Theory(DisplayName = nameof(Activate))]
+    [Trait("Domain", "Genre - Aggregates")]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void Activate(bool isActive)
+    {
+        var genre = _fixture.GetExampleGenre(isActive);
+
+        genre.Activate();
+
+        genre.Should().NotBeNull();
+        genre.IsActive.Should().BeTrue();
+    }
+
+    [Theory(DisplayName = nameof(Deactivate))]
+    [Trait("Domain", "Genre - Aggregates")]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void Deactivate(bool isActive)
+    {
+        var genre = _fixture.GetExampleGenre(isActive);
+
+        genre.Deactivate();
+
+        genre.Should().NotBeNull();
+        genre.IsActive.Should().BeFalse();
+    }
+
+    [Fact(DisplayName = nameof(Update))]
+    [Trait("Domain", "Genre - Aggregates")]
+    public void Update()
+    {
+        var genre = _fixture.GetExampleGenre();
+        var newName = _fixture.GetValidName();
+
+        genre.Update(newName);
+
+        genre.Should().NotBeNull();
+        genre.IsActive.Should().Be(genre.IsActive);
+        genre.Name.Should().Be(newName);    
+    }
+
+    [Theory(DisplayName = nameof(UpdateThrowWhenNameIsEmpty))]
+    [Trait("Domain", "Genre - Aggregates")]
+    [InlineData("")]
+    [InlineData("  ")]
+    [InlineData(null)]
+    public void UpdateThrowWhenNameIsEmpty(string? name)
+    {
+        var genre = _fixture.GetExampleGenre();
+
+        var action =
+            () => genre.Update(name!);
+
+        action.Should().Throw<EntityValidationException>()
+            .WithMessage("Name should not be empty or null");
+    }
+
+    [Fact(DisplayName = nameof(AddCategory))]
+    [Trait("Domain", "Genre - Aggregates")]
+    public void AddCategory()
+    {
+        var genre = _fixture.GetExampleGenre();
+        var categoryGuid = Guid.NewGuid();
+
+        genre.AddCategory(categoryGuid);
+
+        genre.Categories.Should().HaveCount(1);
+        genre.Categories.Should().Contain(categoryGuid);
+    }
+
+    [Fact(DisplayName = nameof(AddTwoCategories))]
+    [Trait("Domain", "Genre - Aggregates")]
+    public void AddTwoCategories()
+    {
+        var genre = _fixture.GetExampleGenre();
+        var categoryGuid1 = Guid.NewGuid();
+        var categoryGuid2 = Guid.NewGuid();
+
+        genre.AddCategory(categoryGuid1);
+        genre.AddCategory(categoryGuid2);
+
+        genre.Categories.Should().HaveCount(2);
+        genre.Categories.Should().Contain(categoryGuid1);
+        genre.Categories.Should().Contain(categoryGuid2);
+    }
+
+
+    [Fact(DisplayName = nameof(RemoveCategory))]
+    [Trait("Domain", "Genre - Aggregates")]
+    public void RemoveCategory()
+    {
+        var exampleGuid = Guid.NewGuid();
+        var genre = _fixture.GetExampleGenre(
+            categoriesIdsList: new List<Guid>()
+            {
+                Guid.NewGuid(),
+                Guid.NewGuid(),
+                exampleGuid,
+                Guid.NewGuid(),
+                Guid.NewGuid()
+            }
+        );
+
+        genre.RemoveCategory(exampleGuid);
+
+        genre.Categories.Should().HaveCount(4);
+        genre.Categories.Should().NotContain(exampleGuid);
+    }
+
+    [Fact(DisplayName = nameof(RemoveAllCategories))]
+    [Trait("Domain", "Genre - Aggregates")]
+    public void RemoveAllCategories()
+    {
+        var genre = _fixture.GetExampleGenre(
+            categoriesIdsList: new List<Guid>()
+            {
+                Guid.NewGuid(),
+                Guid.NewGuid(),
+                Guid.NewGuid(),
+                Guid.NewGuid(),
+                Guid.NewGuid()
+            }
+        );
+
+        genre.RemoveAllCategories();
+
+        genre.Categories.Should().HaveCount(0);
+    }
+}
---
+++ tests/Codeflix.Catalog.UnitTests/Domain/Entity/Genre/GenreTestFixture.cs
@@ -0,0 +1,27 @@
+﻿using Codeflix.Catalog.UnitTests.Common;
+using Xunit;
+using DomainEntity = Codeflix.Catalog.Domain.Entity;
+
+namespace Codeflix.Catalog.UnitTests.Domain.Entity.Genre;
+
+[CollectionDefinition(nameof(GenreTestFixture))]
+public class GenreTestFixtureCollection : ICollectionFixture<GenreTestFixture>
+{ }
+
+public class GenreTestFixture : BaseFixture
+{
+    public string GetValidName()
+        => Faker.Commerce.Categories(1).First();
+
+    public DomainEntity.Genre GetExampleGenre(bool isActive = true, List<Guid>? categoriesIdsList = null)
+    {
+       var genre = new DomainEntity.Genre(GetValidName(), isActive);
+
+        categoriesIdsList?.ForEach(x =>
+            {
+                genre.AddCategory(x);
+            });
+
+        return genre;
+    }
+}
