b61113359b49bcc9bac8bd0e4188496297e741c3
Guilherme2907 <guilherme.magalhaes2907@gmail.com>
2024-12-02 19:12:06 -0300

    Creation of base fixture for genre use cases

---
+++ tests/Codeflix.Catalog.UnitTests/Application/Genre/Common/GenreUseCasesBaseFixture.cs
@@ -0,0 +1,55 @@
+﻿using Codeflix.Catalog.Domain.Repository;
+using Codeflix.Catalog.UnitTests.Common;
+using CodeFlix.Catalog.Application.Interfaces;
+using Moq;
+using DomainEntity = Codeflix.Catalog.Domain.Entity;
+
+namespace Codeflix.Catalog.UnitTests.Application.Genre.Common;
+
+public class GenreUseCasesBaseFixture : BaseFixture
+{
+    public Mock<IGenreRepository> GetGenreRepositoryMock()
+       => new();
+
+    public Mock<IUnitOfWork> GetUnitOfWorkMock()
+        => new();   
+    
+    public Mock<ICategoryRepository> GetCategoryRepositoryMock()
+        => new();
+
+    public string GetValidGenreName()
+           => Faker.Commerce.Categories(1)[0];
+
+    public DomainEntity.Genre GetExampleGenre(
+          bool? isActive = null,
+          List<Guid>? categoriesIds = null
+      )
+    {
+        var genre = new DomainEntity.Genre(
+            GetValidGenreName(),
+            isActive ?? GetRandomBoolean()
+        );
+        categoriesIds?.ForEach(genre.AddCategory);
+        return genre;
+    }
+
+    public List<Guid> GetRandomIdsList(int? count = null)
+       => Enumerable
+           .Range(1, count ?? (new Random()).Next(1, 10))
+           .Select(_ => Guid.NewGuid())
+           .ToList();
+
+    public List<DomainEntity.Genre> GetExampleGenresList(
+       int count = 10
+   )
+       => Enumerable.Range(1, count).Select(_ =>
+       {
+           var genre = new DomainEntity.Genre(
+               GetValidGenreName(),
+               GetRandomBoolean()
+           );
+           GetRandomIdsList()
+               .ForEach(genre.AddCategory);
+           return genre;
+       }).ToList();
+}
