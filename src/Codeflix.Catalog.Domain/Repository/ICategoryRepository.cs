d1b4d82d1b66f2d907da7807d9cbfc17da9fc0f6
Guilherme2907 <guilherme.magalhaes2907@gmail.com>
2024-12-02 19:18:10 -0300

    Revert "Added method to get a valid list of categories ids by a list of ids"
    This reverts commit 4dad774c684ebb60414abd1a418bb1a6c94ea051.

--- src/Codeflix.Catalog.Domain/Repository/ICategoryRepository.cs
+++ src/Codeflix.Catalog.Domain/Repository/ICategoryRepository.cs
@@ -4,13 +4,7 @@ using Codeflix.Catalog.Domain.SeedWork.SearchableRepository;
 
 namespace Codeflix.Catalog.Domain.Repository;
 
-public interface ICategoryRepository
-    : IGenericRepository<Category>,
+public interface ICategoryRepository 
+    : IGenericRepository<Category>, 
     ISeachableRepository<Category>
-{
-    public Task<IReadOnlyList<Guid>> GetIdsListByIds
-    (
-        List<Guid> ids,
-        CancellationToken cancellationToken
-    );
-}
+{}
