2d6a6a19d70f99655d3b0024035d3d00d9595d4f
Guilherme2907 <guilherme.magalhaes2907@gmail.com>
2024-12-02 19:09:30 -0300

    Creation of tests for usecase create genre

---
+++ tests/Codeflix.Catalog.UnitTests/Application/Genre/CreateGenre/CreateGenreTestFixture.cs
@@ -0,0 +1,39 @@
+﻿using Codeflix.Catalog.UnitTests.Application.Genre.Common;
+using CodeFlix.Catalog.Application.UseCases.Genre.CreateGenre;
+using Xunit;
+
+namespace Codeflix.Catalog.UnitTests.Application.Genre.CreateGenre;
+
+
+[CollectionDefinition(nameof(CreateGenreTestFixture))]
+public class CreateGenreTestFixtureCollection : ICollectionFixture<CreateGenreTestFixture>
+{ }
+
+public class CreateGenreTestFixture : GenreUseCasesBaseFixture
+{
+    public CreateGenreInput GetExampleInput()
+         => new (
+             GetValidGenreName(),
+             GetRandomBoolean()
+         );
+
+    public CreateGenreInput GetExampleInput(string name)
+        => new(
+            name,
+            GetRandomBoolean()
+        );
+
+    public CreateGenreInput GetExampleInputWithCategories()
+    {
+        var numberOfCategoriesIds = (new Random()).Next(1, 10);
+        var categoriesIds = Enumerable.Range(1, numberOfCategoriesIds)
+            .Select(_ => Guid.NewGuid())
+            .ToList();
+
+        return new(
+            GetValidGenreName(),
+            GetRandomBoolean(),
+            categoriesIds
+        );
+    }
+}
